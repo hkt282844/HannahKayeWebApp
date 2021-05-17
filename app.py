354cc1cc21c22a5282e7cc30ae88722c2d24ae07
Hannah Terry <hterry@berkeley.edu>
2021-05-17 16:24:05 -0700

    Create database and update .gitignore to ignore .db file

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+import sqlite3
 from flask import Flask, render_template
+
+def get_db_connection():
+  connection = sqlite3.connect('database.db')
+  connection.row_factory = sqlite3.Row
+  return connection
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  connection = get_db_connection()
+  posts = connection.execute('SELECT * FROM posts').fetchall()
+  connection.close()
+  return render_template('index.html', posts=posts)
