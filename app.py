b1bec4c9e26d9181bee062a847e077d7bc520afd
Hannah Terry <hterry@berkeley.edu>
2021-05-17 19:31:20 -0700

    Create posts and ability to navigate

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import sqlite3
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 
 def get_db_connection():
   connection = sqlite3.connect('database.db')
   connection.row_factory = sqlite3.Row
   return connection
+
+def get_post(post_id):
+    connection = get_db_connection()
+    post = connection.execute('SELECT * FROM posts WHERE id = ?',
+                              (post_id,)).fetchone()
+    connection.close()
+    if post is None:
+      abort(404)
+    return post
+
 
 app = Flask(__name__)
 
@@ -14,3 +25,8 @@ def index():
   posts = connection.execute('SELECT * FROM posts').fetchall()
   connection.close()
   return render_template('index.html', posts=posts)
+
+@app.route('/<int:post_id>')
+def post(post_id):
+  post = get_post(post_id)
+  return render_template('post.html', post=post)
